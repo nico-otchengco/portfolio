f8fdf2203fe90b1c82b60e676c6cef2123dd8f75
nico-otchengco <otchengco.nico@gmail.com>
2026-05-04 15:26:12 +0800

    bug fixes

--- my_portfolio/src/main.tsx
+++ my_portfolio/src/main.tsx
@@ -1,6 +1,5 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "./styles/style.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/style.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
